25289bf2cbe589594ba78d14a7eea90f8a61394e
Kaushal <kaushal.v.bhat@gmail.com>
2020-04-11 13:52:18 +0530

    The game is in working condition except the draw.

--- js/game.js
+++ js/game.js
@@ -1,13 +1,53 @@
 
 let checkVal = false; // x = false, o = true
 let repeatClick = false;
+
+let arr = [];
+
+let check = [];
+
+for (let x = 0; x < 9; x++) {
+    arr[x] = 0;
+}
+
+let p1Score = 0;
+let p2Score = 0;
+
+p1Score =  localStorage.getItem("player1Score");
+document.getElementById('player1').setAttribute("data-number", p1Score);
+
+p2Score =  localStorage.getItem("player2Score");
+document.getElementById('player2').setAttribute("data-number", p2Score);
+
+if (p1Score > p2Score) {
+    localStorage.getItem("winnerPlayer");
+    document.getElementById('winner').innerHTML = "Player 1"
+    localStorage.setItem("winnerPlayer", "player2");
+}
+else if (p2Score > p1Score) {
+    localStorage.getItem("winnerPlayer");
+    document.getElementById('winner').innerHTML = "Player 2"
+    localStorage.setItem("winnerPlayer", "player2");
+}
+else {
+    localStorage.getItem("winnerPlayer");
+    document.getElementById('winner').innerHTML = "Draw"
+    localStorage.setItem("winnerPlayer", "draw");
+}
+
 for (let i = 0; i < document.querySelectorAll('img').length; i++) {
     document.querySelectorAll('img')[i].addEventListener("click", function () {
         //console.log(event);
         doThis(i + 1);
+        verifyPlay();
     });
 }
 
+function resetScores() {
+    localStorage.clear();
+    p1Score = 0;
+    p2Score = 0;
+}
 
 function doThis(index) {
     //console.log("The index is " + index);
@@ -16,113 +56,191 @@ function doThis(index) {
     switch(index) {
         case 1: 
             con = document.querySelectorAll('img')[index - 1];
-            console.log(con);
-            if(checkVal) {
+            //console.log(con);
+            if(checkVal && checkTile(index - 1)) {
                 con.src = "images/to.png";
                 checkVal = false;
                 console.log(checkVal);
+                arr[index - 1] = 'o'
+                check[index - 1] = true;
             }
-            else {
+            else if (!checkVal && checkTile(index - 1)) {
                 con.src = "images/tx.png";
                 checkVal = true;
                 console.log(checkVal);
+                arr[index - 1] = 'x'
+                check[index - 1] = false;
+            }
+            else {
+                console.log("Choose some other tile.");
             }
             break;
         case 2: 
             con = document.querySelectorAll('img')[index - 1];
-            console.log(con);
-            console.log(checkVal);
-            if(checkVal) {
+            //console.log(con);
+            if(checkVal && checkTile(index - 1)) {
                 con.src = "images/to.png";
                 checkVal = false;
+                console.log(checkVal);
+                arr[index - 1] = 'o'
+                check[index - 1] = true;
             }
-            else {
+            else if (!checkVal && checkTile(index - 1)) {
                 con.src = "images/tx.png";
                 checkVal = true;
+                console.log(checkVal);
+                arr[index - 1] = 'x'
+                check[index - 1] = false;
+            }
+            else {
+                console.log("Choose some other tile.");
             }
             break;
         case 3: 
             con = document.querySelectorAll('img')[index - 1];
-            console.log(con);
-            if(checkVal) {
+            //console.log(con);
+            if(checkVal && checkTile(index - 1)) {
                 con.src = "images/to.png";
                 checkVal = false;
+                console.log(checkVal);
+                arr[index - 1] = 'o'
+                check[index - 1] = true;
             }
-            else {
+            else if (!checkVal && checkTile(index - 1)) {
                 con.src = "images/tx.png";
                 checkVal = true;
+                console.log(checkVal);
+                arr[index - 1] = 'x'
+                check[index - 1] = false;
+            }
+            else {
+                console.log("Choose some other tile.");
             }
             break;
         case 4: 
             con = document.querySelectorAll('img')[index - 1];
-            console.log(con);
-            if(checkVal) {
+            //console.log(con);
+            if(checkVal && checkTile(index - 1)) {
                 con.src = "images/to.png";
                 checkVal = false;
+                console.log(checkVal);
+                arr[index - 1] = 'o'
+                check[index - 1] = true;
             }
-            else {
+            else if (!checkVal && checkTile(index - 1)) {
                 con.src = "images/tx.png";
                 checkVal = true;
+                console.log(checkVal);
+                arr[index - 1] = 'x'
+                check[index - 1] = false;
+            }
+            else {
+                console.log("Choose some other tile.");
             }
             break;
         case 5: 
             con = document.querySelectorAll('img')[index - 1];
-            console.log(con);
-            if(checkVal) {
+            //console.log(con);
+            if(checkVal && checkTile(index - 1)) {
                 con.src = "images/to.png";
                 checkVal = false;
+                console.log(checkVal);
+                arr[index - 1] = 'o'
+                check[index - 1] = true;
             }
-            else {
+            else if (!checkVal && checkTile(index - 1)) {
                 con.src = "images/tx.png";
                 checkVal = true;
+                console.log(checkVal);
+                arr[index - 1] = 'x'
+                check[index - 1] = false;
+            }
+            else {
+                console.log("Choose some other tile.");
             }
             break;
         case 6: 
             con = document.querySelectorAll('img')[index - 1];
-            console.log(con);
-            if(checkVal) {
+            //console.log(con);
+            if(checkVal && checkTile(index - 1)) {
                 con.src = "images/to.png";
                 checkVal = false;
+                console.log(checkVal);
+                arr[index - 1] = 'o'
+                check[index - 1] = true;
             }
-            else {
+            else if (!checkVal && checkTile(index - 1)) {
                 con.src = "images/tx.png";
                 checkVal = true;
+                console.log(checkVal);
+                arr[index - 1] = 'x'
+                check[index - 1] = false;
+            }
+            else {
+                console.log("Choose some other tile.");
             }
             break;
         case 7: 
             con = document.querySelectorAll('img')[index - 1];
-            console.log(con);
-            if(checkVal) {
+            //console.log(con);
+            if(checkVal && checkTile(index - 1)) {
                 con.src = "images/to.png";
                 checkVal = false;
+                console.log(checkVal);
+                arr[index - 1] = 'o'
+                check[index - 1] = true;
             }
-            else {
+            else if (!checkVal && checkTile(index - 1)) {
                 con.src = "images/tx.png";
                 checkVal = true;
+                console.log(checkVal);
+                arr[index - 1] = 'x'
+                check[index - 1] = false;
+            }
+            else {
+                console.log("Choose some other tile.");
             }
             break;
         case 8: 
             con = document.querySelectorAll('img')[index - 1];
-            console.log(con);
-            if(checkVal) {
+            //console.log(con);
+            if(checkVal && checkTile(index - 1)) {
                 con.src = "images/to.png";
                 checkVal = false;
+                console.log(checkVal);
+                arr[index - 1] = 'o'
+                check[index - 1] = true;
             }
-            else {
+            else if (!checkVal && checkTile(index - 1)) {
                 con.src = "images/tx.png";
                 checkVal = true;
+                console.log(checkVal);
+                arr[index - 1] = 'x'
+                check[index - 1] = false;
+            }
+            else {
+                console.log("Choose some other tile.");
             }
             break;
         case 9: 
             con = document.querySelectorAll('img')[index - 1];
-            console.log(con);
-            if(checkVal) {
+            //console.log(con);
+            if(checkVal && checkTile(index - 1)) {
                 con.src = "images/to.png";
                 checkVal = false;
+                console.log(checkVal);
+                arr[index - 1] = 'o'
+                check[index - 1] = true;
             }
-            else {
+            else if (!checkVal && checkTile(index - 1)) {
                 con.src = "images/tx.png";
                 checkVal = true;
+                console.log(checkVal);
+                arr[index - 1] = 'x'
+                check[index - 1] = false;
+            }
+            else {
+                console.log("Choose some other tile.");
             }
             break;
         default:
@@ -132,8 +250,98 @@ function doThis(index) {
     }
 }
 
-function checkTurn(checkVal1) {
-    if (checkVal1 == 'o') {
+function verifyPlay() {
+    if (check[0] == check[4] && check[0] == check[8] && xOrO(check[0])) {
+        print(check[0]);
+    }
+    else if (check[2] == check[4] && check[2] == check[6] && xOrO(check[2])) {
+        print(check[2]);
+    }
+    else if (check[0] == check[1] && check[0] == check[2] && xOrO(check[0])) {
+        print(check[0]);
+    }
+    else if (check[3] == check[4] && check[3] == check[5] && xOrO(check[3])) {
+        print(check[3]);
+    }
+    else if (check[6] == check[7] && check[6] == check[8] && xOrO(check[6])) {
+        print(check[6]);
+    }
+    else if (check[0] == check[3] && check[0] == check[6] && xOrO(check[0])) {
+        print(check[0]);
+    }
+    else if (check[1] == check[4] && check[1] == check[7] && xOrO(check[1])) {
+        print(check[1]);
+    }
+    else if (check[2] == check[5] && check[2] == check[8] && xOrO(check[2])) {
+        print(check[2]);
+    }
+    else {
+        checkForDraw();
+    }
+}
+
+function checkForDraw() {
+    /*let k;
+    let count = 0;
+    for (k = 0; k < 9; k++) {
+        if (check[k] != 0) {
+            count++;
+        }
+    }
+    if (count == 9) {
+        alert("The round is draw.");
+    }*/
+    if (check[0] == 0 && check[1] == 0 && check[2] == 0 && check[3] == 0 && check[4] == 0 && check[5] == 0 && check[6] == 0 && check[7] == 0 && check[8] == 0 && check[9] == 0) {
+        alert("Draw.");
+        //window.location.reload();
+    }
+    else {
+        console.log("Continue playing...")
+    }
+}
+
+function print(temp1) {
+    if (temp1 == true) {
+        console.log("O wins");
+        updateScore('o');
+    }
+    else if (temp1 == false) {
+        console.log("X wins");
+        updateScore('x');
+    }
+    else {
+        console.log("Error");
+    }
+}
+
+function updateScore(winningChar) {
+    if (winningChar == 'x') {
+        p1Score++;
+        displayUpdateForX();
+    }
+    else if (winningChar == 'o') {
+        p2Score++;
+        displayUpdateForO();
+    }
+    else {
+        console.log("Error finding winner.");
+    }
+}
+
+function displayUpdateForX() {
+    document.getElementById('player1').setAttribute("data-number", p1Score);
+    localStorage.setItem("player1Score", p1Score);
+    window.location.reload();
+}
+
+function displayUpdateForO() {
+    document.getElementById('player2').setAttribute("data-number", p2Score);
+    localStorage.setItem("player2Score", p2Score);
+    window.location.reload();
+}
+
+function xOrO(temp) {
+    if (temp == true || temp == false) {
         return true;
     }
     else {
@@ -141,133 +349,11 @@ function checkTurn(checkVal1) {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const statusDisplay = document.querySelector('.game--status');
-
-let gameActive = true;
-let currentPlayer = "X";
-let gameState = ["", "", "", "", "", "", "", "", ""];
-
-const winningMessage = () => `Player ${currentPlayer} has won!`;
-const drawMessage = () => `Game ended in a draw!`;
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
-
-statusDisplay.innerHTML = currentPlayerTurn();
-
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-    gameState[clickedCellIndex] = currentPlayer;
-    clickedCell.innerHTML = currentPlayer;
-}
-
-function handlePlayerChange() {
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    statusDisplay.innerHTML = currentPlayerTurn();
-}
-
-function handleResultValidation() {
-    let roundWon = false;
-    for (let i = 0; i <= 7; i++) {
-        const winCondition = winningConditions[i];
-        let a = gameState[winCondition[0]];
-        let b = gameState[winCondition[1]];
-        let c = gameState[winCondition[2]];
-        if (a === '' || b === '' || c === '') {
-            continue;
-        }
-        if (a === b && b === c) {
-            roundWon = true;
-            break
-        }
+function checkTile(j) {
+    if (arr[j] == 0) {
+        return true;
     }
-
-    if (roundWon) {
-        statusDisplay.innerHTML = winningMessage();
-        gameActive = false;
-        return;
+    else {
+        return false;
     }
-
-    let roundDraw = !gameState.includes("");
-    if (roundDraw) {
-        statusDisplay.innerHTML = drawMessage();
-        gameActive = false;
-        return;
-    }
-
-    handlePlayerChange();
 }
-
-function handleCellClick(clickedCellEvent) {
-    const clickedCell = clickedCellEvent.target;
-    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
-
-    if (gameState[clickedCellIndex] !== "" || !gameActive) {
-        return;
-    }
-
-    handleCellPlayed(clickedCell, clickedCellIndex);
-    handleResultValidation();
-}
-
-function handleRestartGame() {
-    gameActive = true;
-    currentPlayer = "X";
-    gameState = ["", "", "", "", "", "", "", "", ""];
-    statusDisplay.innerHTML = currentPlayerTurn();
-    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
-}
-
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
-
-*/
